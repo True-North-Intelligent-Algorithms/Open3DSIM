67f7c41eb7152438fb91cac5b0e460c6e076cb03
bnorthan <bnorthan@gmail.com>
2025-02-07 10:14:28 -0500

    Do abs operation as separate step
    Do the abs as a separate step.  We now return the reconstructed data
    with and without the abs operator applied.  While it would be nice to
    have a non-negativity constraint, applying abs at the end could create
    artifacts.  For example images the signa is 3,2,1,0,-1,-2,-1,0,1,2,3
    applying abs would give 3,2,1,0,1,2,1,0,1,2,3.  So it would change the
    shape of the curve to give a 'false' pulse in the middle.

--- MATLAB_Open_3DSIM/help_functions/filter_3D.m
+++ MATLAB_Open_3DSIM/help_functions/filter_3D.m
@@ -1,4 +1,4 @@
-function [final_image] = filter_3D(dataparams,sum_fft,Nx,Ny,Nz,lambdaregul1,lambdaregul2,output)
+function [final_image, final_image_abs] = filter_3D(dataparams,sum_fft,Nx,Ny,Nz,lambdaregul1,lambdaregul2,output)
 
 %% Calculate the Frequency mask
 Mask_final = ones(Nx,Ny,Nz);
@@ -10,15 +10,19 @@ Filter1 = get_filter1(dataparams,lambdaregul).*Mask_final;
 lambdaregul = lambdaregul2;
 Filter2 = get_filter2(dataparams,lambdaregul).*Mask_final;
 
-%% Do the filter
+%% Do the filter (BN do the abs as separate step so we also have reconstructed image without abs applied)
 sum_fft1 = sum_fft.*Filter1.*Filter2;
-final_image = fftshift(abs(ifftn(ifftshift(sum_fft1))));
+final_image = fftshift(ifftn(ifftshift(sum_fft1)));
+final_image_abs = abs(final_image);
 
 %% Save the results
 maxnum = max(max(max(final_image)));
-final_image = uint8(255*final_image./maxnum);
-stackfilename = [output ,'Open_3DSIM.tif'];
-for k = 1:Nz
-    imwrite(final_image(:,:,k), stackfilename, 'WriteMode','append') % Ð´ÈëstackÍ¼Ïñ
-end
+
+% BN comment out conversion to 8 bit and imwrite, this will be done in
+% Open_3DSIM now...
+%final_image = uint8(255*final_image./maxnum);
+%stackfilename = [output ,'Open_3DSIM.tif'];
+%for k = 1:Nz
+%    imwrite(final_image(:,:,k), stackfilename, 'WriteMode','append') % Ð´ÈëstackÍ¼Ïñ
+%end
 
